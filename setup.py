db2b64872157ded5993c8c2f1a515389525c227e
Sagar Giri <sagargiri456@gamil.com>
2024-09-29 05:46:20 -0700

    Add .gitignore, requirements.txt, and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Scrapper",
+    version='0.0.1',
+    author="Sagar_Giri",
+    author_email="sagargiri456@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
